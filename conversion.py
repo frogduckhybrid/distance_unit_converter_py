11dc6e0fb29aa6e2b8a69f1e1ef0324c6f943dcf
frogduckhybrid <frogduckhybrid@gmail.com>
2020-11-29 22:10:38 +0000

    better everything

--- conversion.py
+++ conversion.py
@@ -2,6 +2,7 @@
 
 # Centimetre
 cm_conversions = {
+    'cm' : 1,
     'ft' : 0.0328084,
     'in' : 0.3937008,
     'km' : 0.00001,
@@ -11,10 +12,23 @@ cm_conversions = {
     'yd' : 0.01093613
 }
 
+# Feet
+ft_conversions = {
+    'cm' : 30.48,
+    'ft' : 1,
+    'in' : 12,
+    'km' : 0.0003048,
+    'm' : 0.3048,
+    'mi' : 0.0001893939,
+    'nm' : 0.0001645788,
+    'yd' : 0.3333333333
+}
+
 # Inch
 in_conversions = {
     'cm' : 2.54,
     'ft' : 0.08333333,
+    'in' : 1,
     'km' : 0.0000254,
     'm' : 0.0254,
     'mi' : 0.00001578283,
@@ -22,26 +36,16 @@ in_conversions = {
     'yd' : 0.02777778
 }
 
-# Feet
-ft_conversions = {
-    'cm' : 30.48,
-    'in' : 12,
-    'km' : 0.0003048,
-    'm' : 0.3048,
-    'mi' : 0.0001893939,
-    'nm' : 0.0001645788,
-    'yd' : 0.3333333333,
-}
-
-# Yard
-yd_conversions = {
-    'cm' : 91.44,
-    'ft' : 3,
-    'in' : 36,
-    'km' : 0.0009144,
-    'm' : 0.9144,
-    'mi' : 0.0005681818,
-    'nm' : 0.0004937363
+# Kilometre
+km_conversions = {
+    'cm' : 100000,
+    'ft': 3280.84,
+    'in' : 39370.08,
+    'km' : 1,
+    'm' : 1000,
+    'mi' : 0.62137,
+    'nm' : 0.5399565,
+    'yd' : 1093.613
 }
 
 # Metre
@@ -50,21 +54,12 @@ m_conversions = {
     'ft' : 3.28084,
     'in' : 39.37008,
     'km' : 0.001,
+    'm' : 1,
     'mi' : 0.0006213712,
     'nm' : 0.0005399565,
-    'yd' : 1.093613
+    'yd' : 1.093613,
 }
 
-# Kilometre
-km_conversions = {
-    'cm' : 100000,
-    'ft': 3280.84,
-    'in' : 39370.08,
-    'm' : 1000,
-    'mi' : 0.62137,
-    'nm' : 0.5399565,
-    'yd' : 1093.613
-}
 
 # Mile
 mi_conversions = {
@@ -73,6 +68,7 @@ mi_conversions = {
     'in' : 63360,
     'km' : 1.609344,
     'm' : 1609.344,
+    'mi' : 1,
     'nm' : 0.8689758,
     'yd' : 1760
 }
@@ -85,56 +81,64 @@ nm_conversions = {
     'km' : 1.852001,
     'm' : 1852.001,
     'mi' : 1.15078,
+    'nm' : 1,
     'yd' : 2025.373
 }
+
+# Yard
+yd_conversions = {
+    'cm' : 91.44,
+    'ft' : 3,
+    'in' : 36,
+    'km' : 0.0009144,
+    'm' : 0.9144,
+    'mi' : 0.0005681818,
+    'nm' : 0.0004937363,
+    'yd' : 1
+}
+
 
 # all conversions
 conversions = {
     'cm' : cm_conversions,
-    'in' : in_conversions,
     'ft' : ft_conversions,
-    'yd' : yd_conversions,
-    'm' : m_conversions,
+    'in' : in_conversions,
     'km' : km_conversions,
+    'm' : m_conversions,
     'mi' : mi_conversions,
-    'nm' : nm_conversions
+    'nm' : nm_conversions,
+    'yd' : yd_conversions
 }
 
+unit_names = ['cm', 'ft', 'in', 'km', 'm', 'mi', 'nm', 'yd']
 # Lambdas
-
-convert = lambda value, input_unit, result_unit: value * conversions[input_unit][result_unit]
+convert = lambda value, input_str, result_str: value * conversions[input_str][result_str]
 # print(convert(5, 'mi', 'ft'))
 
-convert_and_print = lambda value, input_unit, result_unit: print(value, input_unit, "=", convert(value, input_unit, result_unit), result_unit)
+convert_and_print = lambda value, input_str, result_str: print(value, input_str, "=", convert(value, input_str, result_str), result_str)
 # convert_and_print(51, 'mi', 'km')
 
 
 # Interactive function
 def convert_interactive():
-    count = 0
     print("Unit to convert from?")
-    for key in conversions:
-        print(count, ": ", key)
-        count += 1
-    print()
+    for i in range(len(unit_names)):
+        print(i, ": ", unit_names[i])
 
-    input_unit = int(input())
-    input_unit_str = list(conversions.keys())[input_unit]
-    input_value = int(input("Enter value in "+ input_unit_str + ": "))
+    input_int = int(input())
+    input_str = unit_names[input_int]
 
-    output_units = list(conversions.values())[input_unit]
+    print("Enter Value in ", end = "")
+    input_value = int(input(input_str + ": "))
 
-    count = 0
-    print("Unit to convert ", input_value, input_unit_str, " to?")
-    for key in output_units:
-        print(count, ":", key)
-        count += 1
-    print()
+    print("Unit to convert ", input_value, input_str, " to?")
+    for i in range(len(unit_names)):
+        print(i, ": ", unit_names[i])
 
-    result_unit = int(input())
-    result_unit_str = list(output_units)[result_unit]
-    result_value = input_value * list(output_units.values())[result_unit]
+    result_int = int(input())
+    result_str = unit_names[result_int]
 
-    print(input_value, input_unit_str, "=", result_value, result_unit_str)
+    convert_and_print(input_value, input_str, result_str);
+
 
 convert_interactive()
